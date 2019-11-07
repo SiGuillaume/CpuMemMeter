8fcb05785473ea7a97ce3fd22c2d555a7979d759
guillaumesibot <guillaumesibot@gmail.com>
2019-11-07 11:25:45 +0100

    first

---
+++ CpuMemMonitor/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace CpuMemMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBarCpu = new System.Windows.Forms.ProgressBar();
+            this.progressBarMem = new System.Windows.Forms.ProgressBar();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.CpuUsage = new System.Windows.Forms.Label();
+            this.MemUsage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cpu Usage";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Memory Usage";
+            // 
+            // progressBarCpu
+            // 
+            this.progressBarCpu.Location = new System.Drawing.Point(12, 25);
+            this.progressBarCpu.Name = "progressBarCpu";
+            this.progressBarCpu.Size = new System.Drawing.Size(373, 31);
+            this.progressBarCpu.TabIndex = 2;
+           
+            // 
+            // progressBarMem
+            // 
+            this.progressBarMem.Location = new System.Drawing.Point(12, 88);
+            this.progressBarMem.Name = "progressBarMem";
+            this.progressBarMem.Size = new System.Drawing.Size(373, 31);
+            this.progressBarMem.TabIndex = 3;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(12, 125);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // CpuUsage
+            // 
+            this.CpuUsage.AutoSize = true;
+            this.CpuUsage.Location = new System.Drawing.Point(337, 9);
+            this.CpuUsage.Name = "CpuUsage";
+            this.CpuUsage.Size = new System.Drawing.Size(21, 13);
+            this.CpuUsage.TabIndex = 5;
+            this.CpuUsage.Text = "0%";
+            // 
+            // MemUsage
+            // 
+            this.MemUsage.AutoSize = true;
+            this.MemUsage.Location = new System.Drawing.Point(337, 72);
+            this.MemUsage.Name = "MemUsage";
+            this.MemUsage.Size = new System.Drawing.Size(21, 13);
+            this.MemUsage.TabIndex = 6;
+            this.MemUsage.Text = "0%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 160);
+            this.Controls.Add(this.MemUsage);
+            this.Controls.Add(this.CpuUsage);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.progressBarMem);
+            this.Controls.Add(this.progressBarCpu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBarCpu;
+        private System.Windows.Forms.ProgressBar progressBarMem;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Label CpuUsage;
+        private System.Windows.Forms.Label MemUsage;
+    }
+}
+
---
+++ CpuMemMonitor/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpuMemMonitor
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer _timer;
+        bool startflag = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _timer = new System.Timers.Timer();
+            _timer.Interval = 2000;
+            _timer.Elapsed += OntimeEvent;
+            _timer.AutoReset = true;
+        }
+
+
+        private void OntimeEvent(object source, System.Timers.ElapsedEventArgs e)
+        {
+           // CheckForIllegalCrossThreadCalls = false;    //UNSAFE
+
+            // Je veux monitorer et obtenir les valeurs du cpu et de la mémmoire
+            int cpuValue = GetCpuValue();
+            int memvalue = GetMemValue();
+
+        
+
+            if (progressBarCpu.InvokeRequired)
+            {
+                progressBarCpu.Invoke(new Action(() => progressBarCpu.Value = cpuValue));
+            }
+            else 
+            {
+                progressBarCpu.Value = cpuValue;
+            }
+
+
+            if (CpuUsage.InvokeRequired)
+            {
+                progressBarCpu.Invoke(new Action(() => CpuUsage.Text = cpuValue.ToString() + " %"));
+            }
+            else
+            {
+                CpuUsage.Text = cpuValue.ToString() + " %";
+            }
+
+
+
+            if (progressBarMem.InvokeRequired)
+                {
+                progressBarMem.Invoke(new Action(() => progressBarMem.Value = memvalue));
+            }
+            else
+            {
+                progressBarMem.Value = memvalue;
+            }
+
+            if (MemUsage.InvokeRequired)
+            {
+                progressBarMem.Invoke(new Action(() => MemUsage.Text = memvalue.ToString() + " %"));
+            }
+
+            else
+            {
+                MemUsage.Text = memvalue.ToString() + " %";
+            }
+
+
+            progressBarCpu.Value = cpuValue;
+            progressBarMem.Value = memvalue;
+
+        }
+
+        private int GetCpuValue()
+        {
+            var CpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            CpuCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            int returnvalue = (int)CpuCounter.NextValue();
+            return returnvalue;
+        }
+
+        private int GetMemValue()
+        {
+            var MemCounter = new PerformanceCounter("Memory", "% Committed Bytes in Use");
+            int returnvalue = (int)MemCounter.NextValue();
+            return returnvalue;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (startflag == false)
+            {
+                startflag = true;
+                _timer.Enabled = true;
+                buttonStart.Text = "Stop";
+            }
+            else
+            {
+                startflag = false;
+                _timer.Enabled = false;
+                buttonStart.Text = "Start";
+            }
+            
+        }
+
+    }
+}
